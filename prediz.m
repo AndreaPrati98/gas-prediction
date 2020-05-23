84bea58f9124a715f2ef42f371f7226b6accaa82
AndreaPrati98 <62189267+AndreaPrati98@users.noreply.github.com>
2020-05-23 14:29:49 +0200

    modificato calcolo della predizione

--- prediz.m
+++ prediz.m
@@ -3,13 +3,18 @@ function m_hat = prediz(week)
 %   Detailed explanation goes here
 
 load model_gasST_group.mat;
+load deviazione.mat;
+load media.mat;
 
+normalized_week = (week - media)/deviazione;
 
 theta = model_gasST_group(:, 1);
 std = model_gasST_group(:, 2);
 
-phi = [1, week];
-m_hat = phi * theta;
+phi = [1, normalized_week];
+m_hat_normalized = phi * theta;
+
+m_hat = (m_hat_normalized * deviazione) + media;
 
 end
 
