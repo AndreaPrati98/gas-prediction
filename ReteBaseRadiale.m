48da1a08f146db767e9515ec1cb882e543dfe31b
GarauCristian <cristian.garau01@universitadipavia.it>
2020-05-23 11:00:21 +0200

    Semplificato e aggiornato algoritmo base radiale, aggiunto istogramma dei dati

--- ReteBaseRadiale.m
+++ ReteBaseRadiale.m
@@ -17,56 +17,75 @@ settimaneIdentificazione = 74;
 settimaneValidazione = 30;
 
 goal = 0.0; % sum-squared error goal
-spread = 250;  % spread constant / larghezza campana
-MN = 7; % numero massimo di neuroni
+spread = 150;  % spread constant / larghezza campana
+MN = 6; % numero massimo di neuroni
 DF = 1; % ogni quanti neuroni mostro mse
 
-sum = 0;
 
+%%Inizializzo la rete
 net = newrb(inputIdentificazione, outputIdentificazione, goal, spread, MN, DF);
-for i = 1:settimaneValidazione
-    inputSettimana = inputValidazione(:, i);
-    if i == 1
-        predizione = sim(net, inputSettimana);
-        outputVero = outputValidazione(1,i);
-        errore = outputVero - predizione;
-        vectErrore = errore;
-        sum = (errore)^2;
-    elseif(i < settimaneValidazione)
-        predizione = [predizione, sim(net, inputSettimana)];
-        outputVero = [outputVero, outputValidazione(1,i)];
-        errore = outputVero(i) - predizione(i);
-        vectErrore = [vectErrore, errore];
-        sum = sum + (outputVero(i)-predizione(i))^2;
-    end 
 
-end
-
+predizione = sim(net, inputValidazione);
+vectErrore = outputValidazione-predizione;
+SSR = sum(vectErrore.^2);
 deviation = std(vectErrore);
 
-outString = sprintf("Neu: %d, Spread: %d, RSS: %1.6f, SD: %1.6f\n", MN, spread, sum, deviation)
+outString = sprintf("Neu: %d, Spread: %d, RSS: %1.6f, SD: %1.6f\n", MN, spread, SSR, deviation)
 
+%Ricavo il grafico partendo dai dati di identificazione
+predizioneId = sim(net, inputIdentificazione);
+
+%%Plot delle previsioni normalizzate
 figure(1)
 subplot(1,2,1);
-plot(1:length(outputVero), outputVero);
+scatter(1:length(outputValidazione), outputValidazione, 'o');
 hold on
-plot(1:length(outputVero), predizione);
+scatter(1:length(outputValidazione), predizione, 'x');
 hold off
-legend('Dati reali','previsione')
+legend('Dati veri','previsione')
 
 subplot(1,2,2);
-plot(1:length(outputVero), vectErrore);
+scatter(1:length(outputValidazione), vectErrore);
 hold off
 max(abs(vectErrore));
 min(abs(vectErrore));
 
-% %denormalizzo
-% figure(2)
-% subplot(1,2,1);
-% plot(1:length(outputVero), outputVero*deviazione+media);
-% hold on
-% plot(1:length(outputVero), predizione*deviazione+media);
-% legend('Dati reali','previsione')
-% 
-% subplot(1,2,2);
-% plot(1:length(outputVero), vectErrore);
+%%Plot delle previsioni denormalizzate
+figure(2)
+subplot(1,2,1);
+scatter(1:length(outputValidazione), outputValidazione*deviazione+media, 'o');
+hold on
+scatter(1:length(outputValidazione), predizione*deviazione+media, 'x');
+legend('Dati reali','previsione')
+title('Previsione sui dati di validazione');
+
+subplot(1,2,2);
+scatter(1:length(outputValidazione), (outputValidazione*deviazione+media)-(predizione*deviazione+media));
+title('Residui non normalizzati');
+
+%%Plot della previsione in identificazione
+figure(3);
+plot(1:length(outputIdentificazione), outputIdentificazione);
+hold on
+plot(1:length(predizioneId), predizioneId);
+title('Predizione della funzione in identificazione');
+%view(net)
+
+%%Plot dell'istogramma dei residui
+figure(4);
+vectDati = sort(vectErrore);
+
+range = linspace(-0.3, 0.2, 20)
+somme = zeros(length(range));
+somme = somme(:, 1);
+
+for j = 1:length(vectDati)
+    for i = 1:length(range)
+        if vectDati(j)<range(i)
+            somme(i) = somme(i) + 1;
+            break;
+        end
+    end
+end
+bar(range, somme, 0.9);
+title('istogramma dei residui');
---
+++ istogrammaDati.m
@@ -0,0 +1,30 @@
+clc
+clear
+
+load gasDataSet.mat;
+xString = 'giornoAnno';
+yString = 'giornoSettimana';
+zString = 'dati';
+gasDataSet.Properties.VariableNames = {xString, yString, zString};
+
+vectGiornoAnno = gasDataSet.giornoAnno;
+vectGiornoSettimana = gasDataSet.giornoSettimana;
+vectDati = gasDataSet.dati;
+vectDati = sort(vectDati);
+
+range = linspace(0, 250, 26)
+somme = zeros(length(range));
+somme = somme(:, 1);
+
+for j = 1:length(vectDati)
+    for i = 1:length(range)
+        if vectDati(j)<range(i)
+            somme(i) = somme(i) + 1;
+            break;
+        end
+    end
+end
+
+figure(1);
+bar(range, somme, 0.9);
+%'FaceColor', '#EDB120' 
